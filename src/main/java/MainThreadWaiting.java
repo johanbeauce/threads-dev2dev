f0cb0bb95da67cabd8e583516e4fd999fa24cf8c
Johan Beaucé <jbeauce@pictet-technologies.com>
2024-02-11 22:00:55 +0100

    put the main thread waiting until end of another thread

---
+++ src/main/java/MainThreadWaiting.java
@@ -0,0 +1,20 @@
+public class MainThreadWaiting {
+    private static int counter = 0;
+    public static void main(String[] args) {
+        new Thread(() -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter++;
+            }
+        }).start();
+        while (counter < 1_000_000) {
+            System.out.println("Not reached yet");
+            try {
+                Thread.sleep(1_000);
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Reached counter of " + counter);
+    }
+}
