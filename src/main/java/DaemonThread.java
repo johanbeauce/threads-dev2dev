bdcd151e4338447c2edf5ae4f056e0500949c999
Johan Beaucé <jbeauce@pictet-technologies.com>
2024-02-11 21:47:33 +0100

    Daemon Thread

---
+++ src/main/java/DaemonThread.java
@@ -0,0 +1,17 @@
+public class DaemonThread {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(10_000);
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread finished!");
+        });
+        thread.setDaemon(true);
+        thread.start();
+        System.out.println("Main Thread finished!");
+    }
+}
