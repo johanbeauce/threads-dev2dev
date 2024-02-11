dd52771eb6b684ebca4577086197bb712b890878
Johan Beaucé <jbeauce@pictet-technologies.com>
2024-02-11 21:27:52 +0100

    Create a simple Thread and run it

---
+++ src/main/java/ClassicThread.java
@@ -0,0 +1,13 @@
+public class ClassicThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Hello from a thread!");
+            }
+        });
+        thread.start();
+
+        Thread threadWithLambda = new Thread(() -> System.out.println("Hello from a thread with lambda!"));
+        threadWithLambda.start();
+    }
+}
