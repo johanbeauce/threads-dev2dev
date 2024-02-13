3fb8ed32e20fa95de8f136ba33b2a28e92719f64
Johan Beaucé <jbeauce@pictet-technologies.com>
2024-02-13 22:19:47 +0100

    Using Futur.get will stop the execution until the thread is finish

---
+++ src/main/java/com/beauce/threads/ocp/UsingFutureGet.java
@@ -0,0 +1,32 @@
+package com.beauce.threads.ocp;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class UsingFutureGet {
+    private static int counter = 0;
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        try {
+            Future<?> result = service.submit(() -> {
+                for (int i = 0; i < 1_000_000; i++) counter++;
+            });
+            result.get(10, TimeUnit.SECONDS); // Returns null for Runnable
+            System.out.println("Reached!");
+        }
+        catch (TimeoutException e) {
+            System.out.println("Not reached in time");
+        }
+        catch (ExecutionException | InterruptedException e) {
+            System.out.println("Execution fails:" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        finally {
+            service.shutdown();
+        }
+    }
+}
